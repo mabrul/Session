7978611da83e13bb162bae63aafc1b878e95fd08
IONBOT <113283570+ionmusic@users.noreply.github.com>
2023-11-04 02:46:32 +0700

    Update must_join.py

--- Naya/must_join.py
+++ Naya/must_join.py
@@ -2,6 +2,7 @@ from config import MUST_JOIN
 from pyrogram import Client, filters
 from pyrogram.types import *
 from pyrogram.errors import *
+from pyrogram.types import InlineKeyboardButton
 
 MUST_JOIN = "https://t.me/supprotrewe"
 MUST_JOIN = "https://t.me/nunagabut2"
@@ -22,9 +23,10 @@ async def must_join_channel(bot: Client, msg: Message):
                 await msg.reply(
                     f"Si Anjeng, Masuk Sini Dulu Lu Bangsat !",
                     disable_web_page_preview=True,
-                    reply_markup=InlineKeyboardMarkup([
+                    reply_markup=InlineKeyboardMarkup(
+                    [
                         [InlineKeyboardButton("Sini Nyet Masuk, Jangan Lupa Salam", url="https//:t.me/nunagabut2)]
-                    ])
+                    ]
                 )
                 await msg.stop_propagation()
             except UserBannedInChannel:
